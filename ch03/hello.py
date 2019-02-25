49d7b4f081975b56de0581c4f9a950d66061bb7c
yong.wang <yong.wang@ucarinc.com>
2019-02-25 20:33:45 +0800

    模版变量说明 Jinja2能识别所有的类型的变量,甚至是一些复杂的类型，例如列表，字典和对象

--- ch03/hello.py
+++ ch03/hello.py
@@ -2,6 +2,7 @@
 # encoding:utf-8
 
 from flask import Flask, render_template
+from ch03.my_obj import MyObj
 
 app = Flask(__name__)
 
@@ -18,6 +19,19 @@ def user(name):
     lgr = "User's name is {}".format(name)
     print(lgr)
     return render_template('user.html', name=name)
+
+
+@app.route('/sup')
+def supJinja():
+    # 字典
+    mydict = {'key': 'WY'}
+    # 列表
+    mylist = [1, 2, 3, 4]
+    myintvar = 2
+    print(mydict)
+    myobj = MyObj()
+
+    return render_template('sup.html', mydict=mydict, mylist=mylist, myintvar=myintvar, myobj=myobj)
 
 
 if __name__ == '__main__':
